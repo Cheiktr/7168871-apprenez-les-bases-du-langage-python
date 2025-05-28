a740a3b768a65eb4796d3af31ace94ddc9659ea1
Cheiktr <chiektr@gmail.com>
2025-05-28 18:35:13 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+nom = "sekou"
+age = 28
+taille = 1.75
+etudiant = Oui 
+print(f"bonjour je m'appel {nom} et j'ai {age} avec une taille de {taille} et {etudiant} je suis etudiant.")
+type = nom
+type = age
+type = etudiant
